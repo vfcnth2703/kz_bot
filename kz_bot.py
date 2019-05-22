b0cf9e8d4649f3eb830defd06629fed840e65a1e
vfcnth2703 <av.lobachev@gmail.com>
2019-05-22 21:41:07 +0300

    edit gitignore

--- kz_bot.py
+++ kz_bot.py
@@ -16,29 +16,30 @@ proxy = {'http':'167.86.97.101:80'}
 
 
 def get_html(url):
-    req = requests.get(url,proxies=proxy)
-    return {'html':req.text, 'status':req.status_code}
+    req = requests.get(url, proxies=proxy)
+    return {'html': req.text, 'status':req.status_code}
+
 
 def get_soup(html):
     if html['status'] == 200:
-        soup: BeautifulSoup = BeautifulSoup(html['html'],'lxml')
+        soup: BeautifulSoup = BeautifulSoup(html['html'], 'lxml')
         return soup
     else:
         return None
 
+
 def get_kz_table(soup):
-    table: object = soup.find('table',{'class':'t_peer'})
+    table: object = soup.find('table', {'class': 't_peer'})
     return table
 
 
 def get_today_rows(table):
-    search_res = table.findChildren('td',text = re.compile('сегодня'))
+    search_res = table.findChildren('td', text = re.compile('сегодня'))
     rows: List[object] = []
     for item in search_res:
         assert isinstance(item.parent, object)
         rows.append(item.parent)
     return rows
-
 
 
 def main():
